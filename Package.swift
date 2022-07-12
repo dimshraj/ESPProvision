64b68be662878ca1b834fc50f0cdb53bb0af6561
dimshraj <52859161+dimshraj@users.noreply.github.com>
2022-07-12 21:54:00 +0500

    Initial

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+
+let package = Package(
+    name: "ESPProvision",
+    products: [
+        .library(
+            name: "ESPProvision",
+            targets: ["ESPProvision","ESPProvisionObjC"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.1.0"),
+        .package(url: "https://github.com/christophhagen/Curve25519.git" , from: "1.5.0"),
+    ],
+
+    targets: [
+        .target(
+       name: "ESPProvision",
+       dependencies: ["Curve25519", "swift-protobuf"],
+       path: "Sources/ESPProvision"),
+        .target(
+       name: "ESPProvisionObjC",
+	dependencies: ["Curve25519", "swift-protobuf"],
+       path: "Sources/ESPProvisionObjC"),
+    ]
+)
